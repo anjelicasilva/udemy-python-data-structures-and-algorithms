15479aabc505a24e3f1e148d323f195354d76b2f
Anjelica Silva <anjelicacsilva@gmail.com>
2020-07-27 19:14:40 -0700

    Include solution for array sequence problem: Array Pair Sum

--- array-sequences/array-pair-sum.py
+++ array-sequences/array-pair-sum.py
@@ -8,5 +8,23 @@ def pair_sum(num_list, k):
     return count
 
 
+def pair_sum_solution(arr, k):
+    #Edge Case
+    if len(arr) < 2:
+        return
+    
+    seen = set()
+    output = set()
+
+    for num in arr:
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add( (min(num, target)), max((num, target)) )
+        
+    return len(output)
+
 
 
