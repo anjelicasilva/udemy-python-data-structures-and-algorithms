09bf08833eb812535d64399fd07080fa6ea1eed2
Anjelica Silva <anjelicacsilva@gmail.com>
2020-07-30 17:38:45 -0700

    Include solution for array sequence problem: Find the Missing Element

--- array-sequences/find-the-missing-element.py
+++ array-sequences/find-the-missing-element.py
@@ -11,3 +11,18 @@ def finder(arr1, arr2):
             num_dict[number] -= 1
         else:
             return number
+
+# Solution
+
+def finder(arr1,arr2):
+    # Sort the arrays
+    arr1.sort()
+    arr2.sort()
+    
+    # Compare elements in the sorted arrays
+    for num1, num2 in zip(arr1,arr2):
+        if num1!= num2:
+            return num1
+    
+    # Otherwise return last element
+    return arr1[-1]
