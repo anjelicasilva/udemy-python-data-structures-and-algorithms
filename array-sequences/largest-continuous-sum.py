c6baedc5641b04c0044b46dcf018263931470316
Anjelica Silva <anjelicacsilva@gmail.com>
2020-08-03 19:26:58 -0700

    Write function for array sequences problem: Largest Continuous Sum

--- array-sequences/largest-continuous-sum.py
+++ array-sequences/largest-continuous-sum.py
@@ -1,4 +1,11 @@
 # Given an array of integers (positive and negative) find the largest continuous sum.
 
 def large_cont_sum(arr): 
-    pass
+
+    largest_sum = 0
+    current_sum = 0
+    for num in arr:
+        current_sum += num
+        if current_sum > largest_sum:
+            largest_sum = current_sum
+    return largest_sum
